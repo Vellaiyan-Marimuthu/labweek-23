1725e184d88d3eca80776abe637822cd1159b933
Aswin <aswin@ideas2it.com>
2023-07-20 20:12:01 +0530

    Change in nonce

--- app/api/nonce/route.js
+++ app/api/nonce/route.js
@@ -2,10 +2,28 @@ import { NextResponse } from "next/server";
 
 const { generateNonce } = require("siwe");
 
+// export async function GET(req, res) {
+//   try {
+//     const nonce = generateNonce();
+//     return NextResponse.json({ message: "ok", data: nonce }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         message: "Error",
+//         error,
+//       },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
+
+
 export async function GET(req, res) {
   try {
-    const nonce = generateNonce();
-    return NextResponse.json({ message: "ok", data: nonce }, { status: 200 });
+    const result = await fetch('https://locksmith.unlock-protocol.com/v2/auth/nonce', {})
+    return result;
   } catch (error) {
     return NextResponse.json(
       {
